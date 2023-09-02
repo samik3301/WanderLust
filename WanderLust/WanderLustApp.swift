c2680a1d94c461e0e71e17dc4959c7f1b200b32e
samik3301 <samikx1120@gmail.com>
2023-09-02 11:28:05 +0530

    Initial Commit

---
+++ WanderLust/WanderLustApp.swift
@@ -0,0 +1,17 @@
+//
+//  WanderLustApp.swift
+//  WanderLust
+//
+//  Created by Samik Saraswat on 02/09/23.
+//
+
+import SwiftUI
+
+@main
+struct WanderLustApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
